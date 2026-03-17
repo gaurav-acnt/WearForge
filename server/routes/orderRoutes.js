6223c40cadb2808d1bb8c6c0bd110df8ba3b2a80
gaurav <gauravgs.1290@gmail.com>
2026-03-17 22:56:36 +0530

    fix order issue

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -4,7 +4,7 @@ const { createOrder, getMyOrders,getAllOrders, updateOrderStatus } = require("..
 const isAdmin = require("../middleware/isAdmin");
 const router = express.Router();
 
-router.post("/",auth,createOrder);
+router.post("/create-order",auth,createOrder);
 router.get("/my-orders",auth,getMyOrders);
 router.get("/", auth, isAdmin, getAllOrders);  
 router.put("/:id",auth,isAdmin,updateOrderStatus);
